e1ada48bc3a6f2c9b46ae08cc91ba90511e2b2e6
Sarthak7teen <sarthakvaykar9@gmail.com>
2026-02-26 20:20:24 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -29,11 +29,7 @@ git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaom
 
 git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
 
-#git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings
-
-#git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
-
-#git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
+git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
 
 
 . build/envsetup.sh
