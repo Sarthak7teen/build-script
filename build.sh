ed2415ee786c5b7bf158e5f0cbeb7844d5ad0600
Sarthak7teen <sarthakvaykar9@gmail.com>
2026-02-27 15:26:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -31,7 +31,8 @@ git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
 
-
-. build/envsetup.sh
-axion munch userdebug va
+cd hard*/line*/compat/ &&git fetch https://github.com/LineageOS/android_hardware_lineage_compat refs/changes/04/447604/1 && git cherry-pick FETCH_HEAD && cd -
+source build/envsetup.sh
+gk -s
+axion munch va
 ax -br
